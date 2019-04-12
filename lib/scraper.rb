68dab6d2116229b7f918238d25b14996145210dc
Carden Z <ephemere@protonmail.com>
2019-04-12 04:13:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,12 +25,10 @@ class Scraper
   def get_courses
     doc = get_page
     binding.pry
-    title = doc.css(".post h2").text #h2
-    # schedule = doc.css(".date.text") #.date
-    # description = doc.css(".post") #p
-    #atricle.post.h2 - title
-    #atricle.post.date - schedule
-    #atricle.post.p - description
+    titles = doc.css(".post h2")
+    schedules = doc.css(".post .date")
+    description = doc.css(".post p")
+
   end
 
 end
